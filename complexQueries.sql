131963c50a679d6bc83ae2de907203fd310bea12
Shourya-Srivastava2000 <55703732+Shourya-Srivastava2000@users.noreply.github.com>
2021-03-10 07:30:10 -0800

    Update complexQueries.sql

--- complexQueries.sql
+++ complexQueries.sql
@@ -1,23 +1,17 @@
--- Ranking the most popular genres for games for customers in EACH COUNTRY?
-
-
 CREATE VIEW Popular_Genres
 AS
-    SELECT *
-    FROM Genre AS G, GameGenre AS GG, tblGame AS GA, orderGame AS OG, tblOrder AS O, tblCustomer AS C, Country AS CO
+    SELECT CO.CountryID, G.GenreID, G.GenreName, SUM(OG.Quantity * GA.Price) AS Revenue
+    FROM Genre AS G, GameGenre AS GG, tblGame AS GA, orderGame AS OG, tblOrder AS O, tblCustomer AS C, tblCountry AS CO
     WHERE G.GenreID = GG.GenreID
         AND GG.GameID = GA.GameID
         AND GG.GameID = OG.GameID
         AND OG.OrderID = O.OrderID
         AND C.CustomerID = O.CustomerID
         AND C.CountryID = CO.CountryID
-    GROUP BY G.GenreID, GG.Genre
+    GROUP BY CO.CountryID, G.GenreID, G.GenreName
 GO
-SELECT * 
+SELECT *
 FROM (
-SELECT GenreID, Genre, RANK() OVER(PARTITION BY CountryName ORDER BY SUM(Quantity * Price)) AS RANK
-FROM Popular_Genres)
-WHERE RANK > 5
-
-
-
+SELECT GenreID, GenreName, RANK() OVER(PARTITION BY CountryID ORDER BY Revenue) AS RANK
+    FROM Popular_Genres) as t
+WHERE RANK < 5
