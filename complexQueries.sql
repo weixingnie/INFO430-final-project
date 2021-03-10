535682949a66fdfa77449386fc843633e3a50ecc
Shourya-Srivastava2000 <55703732+Shourya-Srivastava2000@users.noreply.github.com>
2021-03-10 11:07:14 -0800

    Update complexQueries.sql

--- complexQueries.sql
+++ complexQueries.sql
@@ -15,3 +15,24 @@ FROM (
 SELECT GenreID, GenreName, RANK() OVER(PARTITION BY CountryID ORDER BY Revenue) AS RANK
     FROM Popular_Genres) as t
 WHERE RANK < 5
+
+
+CREATE VIEW Top_Rated_games
+AS 
+WITH CTE_TOP_RATED_GAMES(GameID, totalRating) AS (
+    SELECT G.gameID, SUM(RA.RatingNum)
+    FROM tblRATING RA
+    JOIN tblReview RE ON RA.RatingID = RE.RatingID
+    JOIN OrderGame OG ON OG.OrderGameID = RE.OrderGameID
+    JOIN tblGAME G ON G.gameID = OG.GameID
+    GROUP BY G.gameID
+)
+SELECT C.ConsoleName, CTRG.GameID, CTRG.totalRating, 
+        RANK() OVER(PARTITION BY C.ConsoleName ORDER BY CTRG.totalRating) AS Rank
+FROM CTE_TOP_RATED_GAMES AS CTRG
+JOIN tblGameConsole GC ON CTRG.GameID = GC.gameID
+JOIN tblCONSOLE C ON C.ConsoleID = GC.consoleID
+GO
+SELECT *
+FROM Top_Rated_games
+WHERE Rank < 5
